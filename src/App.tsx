8a14900b78b16b402255b14a2a98b5aa348b51dd
WilliamFilh0 <williamfilho8908@gmail.com>
2023-09-15 18:52:26 -0300

    Layout quase competo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
+import { Github } from 'lucide-react'
+import { Button } from "./components/ui/button";
+import { Textarea } from './components/ui/textarea';
+
 export function App() {
 
   return (
-     <h1>Hello world</h1>
+    <div className=' min-h-screen flex flex-col' >
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">upload.ai</h1>
+
+        <div className="flex items-center gap-3">         
+          <Button variant='outline'>
+            <Github className='w-4 h-4 mr-2'/>
+            Git Hub
+          </Button>
+        </div>
+      </div>
+
+      <main className=' flex-1 p-6 flex gap-6' >
+         <div className='flex flex-col flex-1 gap-4'>
+            <div className='grid grid-rows-2 gap-4 flex-1'>
+              <Textarea placeholder='Inclua o prompt pra a IA...' className='resize-none p-4 leading-relaxed'/>
+              <Textarea placeholder='Resultado gerado pela IA...' readOnly className='resize-none p-4 leading-relaxed'/>
+              
+            </div>
+
+            <p className='text-sm text-muted-foreground'>
+              Lembre-se: você pode utilizar a variável <code className='text-blue-400'>{'{transcription}'}</code> no seu promp para adicionar o conteúdo da transcrição do video selecionado
+            </p>
+
+         </div>
+
+         <aside className='w-80 space-y-6'>
+            <form className='space-y-6'>
+              <label 
+              htmlFor="video" 
+              className='border flex rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5'
+              >
+                Selecione um vídeo
+              </label>
+
+              <input type="file" id='video' accept='video/mp4' className='sr-only' />
+            </form>
+         </aside>
+      </main>
+    </div>
+
+    
+
   )
 }
 
